196fad660a119831e4963597bcd71c9018316e38
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-08-27 13:20:24 -0600

    Update index.php
    Se agregó una línea para separar los menús. Se colocaron menús estáticos y no desplegables

--- menu_inicial/index.php
+++ menu_inicial/index.php
@@ -32,7 +32,7 @@
     ?>
     <!--Muestra un pequeño banner donde se muestra el cierre de sesion y la
     informacion del usuario que esta activo en el sistema-->
-  <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <nav class="navbar bg-body-tertiary border-bottom border-body">
     <div class="container-fluid">
     <a class="navbar-brand" href="../funciones_php/Logout.php">Cerrar sesion</a>
     <a class="navbar-brand"><?php echo "Usuario: ". $_SESSION['id'].", ".$_SESSION['usuario'].", ".$_SESSION['tipo']; ?></a>
@@ -43,166 +43,119 @@
     <div class="container text-center">
       <!--Esto es para alinearlos horizontalmente-->
       <div class="row justify-content-center align-items-center g-2">
-            <!-- El codigo siguiente pertenece al primer boton
+            <!-- El codigo siguiente pertenece al primer boton-->
 
-            <div class="col d-flex justify-content-center align-self-start"><a
-                name="inspeccion"
-                id="botones"
-                class="btn btn-lg"
-                href="../inspecciones/"
-                role="button"
-            >Inspeccion diaria</a>
-            </div> -->
-
-
-            <?php
-          
-              echo
-                '<div class="col">
-                <a
-                  name="Inspeccion"
+            <div class="col align-self-start">
+              <div class="col d-flex justify-content-center align-self-start"><a
+                  name="inspeccion"
                   id="botones"
                   class="btn btn-lg"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#contenidoInspeccion"
+                  href=""
                   role="button"
-                  >Inspeccion diaria</a
-                >
-                <div class="collapse" id="contenidoInspeccion">
-                  <br>
-                  <div class="container">
-                  <a
-                    name="Entrada_inspeccion"
-                    id="Calendario_mantenimiento"
-                    class="btn btn-primary"
-                    href="../inspecciones/Verificacion_sistemas/"
-                    role="button"
-                    >Entrada nueva</a
-                  >
-                  </div>
-                  <br>
-                  <div class="container">
-                  <a
-                    name="Historial_ispecciones"
-                    id="Historial_inspecciones"
-                    class="btn btn-primary"
-                    href="../inspecciones/Historial_Inspecciones/"
-                    role="button"
-                    >Historial</a
-                  >
-                  </div>
-                </div>
-                
-              </div>';
+              >Inspeccion diaria</a>
+              </div> 
+
+              
+              <div class="row d-flex justify-content-center align-self-start"><a
+                  name="EntradaNueva"
+                  id="botones_desp"
+                  class="btn btn-lg"
+                  href="../inspecciones/Verificacion_sistemas"
+                  role="button"
+              >Entrada nueva</a>
+              </div>
+
             
-            ?>
-          
-          <!--El codigo siguiente pertenece al segundo boton-->
-
-          <!-- <div class="col d-flex justify-content-center align-self-start"><a
-              name="Mantenimiento"
-              id="botones"
-              class="btn btn-lg"
-              href="../mantenimientos/"
-              role="button"
-          >Mantenimiento</a> -->
-
-          <?php
-          
-            echo
-            '<div class="col">
-            <a
-              name="Mantenimiento"
-              id="botones"
-              class="btn btn-lg"
-              data-bs-toggle="collapse"
-              data-bs-target="#contenidoCalendario"
-              role="button"
-              >Mantenimiento</a
-            >
-            <div class="collapse" id="contenidoCalendario">
-              <br>
-              <div class="container">
-              <a
-                name="Calendario_manenimiento"
-                id="Calendario_mantenimiento"
-                class="btn btn-primary"
-                href="../mantenimientos/Calendario/"
-                role="button"
-                >Calendario de mantenimiento</a
-              >
-              </div>
-              <br>
-              <div class="container">
-              <a
-                name="Entrada_infraestructura"
-                id="Entrada_infraestructura"
-                class="btn btn-primary"
-                href="../mantenimientos/Entrada_infraestructura/"
-                role="button"
-                >Preservacion de Infraestructura</a
-              >
-              </div>
-              <br>
-              <div class="container">
-              <a
-                name="Entrada_calendario"
-                id="Entrada_calendario"
-                class="btn btn-primary"
-                href="../mantenimientos/Entrada_calendario/"
-                role="button"
-                >Entrada calendario</a
-              >
-              </div>
+              <div class="row d-flex justify-content-center align-self-start"><a
+                  name="Historial"
+                  id="botones_desp"
+                  class="btn btn-lg"
+                  href="../inspecciones/Historial_inspecciones"
+                  role="button"
+              >Historial</a>
+              </div> 
+      
+            <!--Aqui tiene que ir la linea de division entre columnas-->
+             <hr class="hr1">
+            <!-- *************************************************** -->
             </div>
             
-          </div>';
-          
-          ?>
+            <!--El codigo siguiente pertenece al segundo boton-->
+            <div class="col align-self-start">
+              <div class="col d-flex justify-content-center align-self-start"><a
+                    name="Mantenimiento"
+                    id="botones"
+                    class="btn btn-lg"
+                    href=""
+                    role="button"
+                >Mantenimiento</a>
+              </div>
 
-          </div>
+            
+              <div class="row d-flex justify-content-center align-self-start"><a
+                    name="Calendario"
+                    id="botones_desp"
+                    class="btn btn-lg"
+                    href="../mantenimientos/Calendario"
+                    role="button"
+                >Calendario de mantenimiento</a>
+              </div>
+
+              <div class="row d-flex justify-content-center align-self-start"><a
+                    name="EntradaCalendario"
+                    id="botones_desp"
+                    class="btn btn-lg"
+                    href="../mantenimientos/Entrada_nueva"
+                    role="button"
+                >Entrada nueva</a>
+              </div>
+
+              <div class="row d-flex justify-content-center align-self-start"><a
+                    name="EntradaInfraestructura"
+                    id="botones_desp"
+                    class="btn btn-lg"
+                    href="../mantenimientos/Entrada_infraestructura"
+                    role="button"
+                >Preservacion de infraestructura</a>
+              </div>
+              <hr class="hr2">
+            </div>
           <!--El codigo siguiente pertenece al tercer boton, donde primero verifica que tipo de 
           usuario es, al verificar el tipo, despliega el boton de usuarios solo si es administrador-->
           <?php
-          if($_SESSION['tipo'] == "Administrador"){
-            echo
-            '<div class="col">
-            <a
-              name="Usuarios"
-              id="botones"
-              class="btn btn-lg"
-              data-bs-toggle="collapse"
-              data-bs-target="#contenidoUsuarios"
-              role="button"
-              >Usuarios</a
-            >
-            <div class="collapse" id="contenidoUsuarios">
-              <br>
-              <div class="container">
-              <a
-                name="Reg_usuario"
-                id="Registrar_usuario"
-                class="btn btn-primary"
-                href="../Registrar_usuario/"
-                role="button"
-                >Registrar usuario</a
-              >
-              </div>
-              <br>
-              <div class="container">
-              <a
-                name="tabla_usuarios"
-                id="tabla_usuarios"
-                class="btn btn-primary"
-                href="../Lista_usuarios/"
-                role="button"
-                >Tabla de usuarios</a
-              >
-              </div>
-            </div>
-            
-          </div>';
-          }
+            if($_SESSION['tipo'] == "Administrador"){
+              echo'
+              <div class="col align-self-start">
+                <div class="col d-flex justify-content-center align-self-start"><a
+                      name="Usuario"
+                      id="botones"
+                      class="btn btn-lg"
+                      href=""
+                      role="button"
+                  >Usuario</a>
+                </div>
+
+              
+                <div class="row d-flex justify-content-center align-self-start"><a
+                      name="RegistrarUsuario"
+                      id="botones_desp"
+                      class="btn btn-lg"
+                      href="../Registrar_usuario"
+                      role="button"
+                  >Registrar usuario</a>
+                </div>
+
+                <div class="row d-flex justify-content-center align-self-start"><a
+                      name="ListaUsuarios"
+                      id="botones_desp"
+                      class="btn btn-lg"
+                      href="../mantenimientos/Lista_usuarios"
+                      role="button"
+                  >Lista de usuarios</a>
+                </div>
+
+              </div>';
+            }
           ?>
       </div>
     </div>
