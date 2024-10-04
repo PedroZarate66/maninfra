702503b980694df26e4a51f59c00b2bd9f4bfdc1
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-04 15:02:16 -0600

    Formulario responsivo

--- mantenimientos/Entrada_infraestructura/index.php
+++ mantenimientos/Entrada_infraestructura/index.php
@@ -62,7 +62,8 @@
         </div>
     </div>
 
-  <body>
+           <!--COMIENZA EL CONTENIDO PRINCIPAL-->
+    <body>
         <main class="content">
         
             <div class="container-fluid mt-3">
@@ -118,7 +119,7 @@
                 }
                 
                 ?>
-                <br>
+            
 
                 <div class="txt-title">
                     <h3>Actualización, Mejoras y Preservacion de Infraestructura</h3>
@@ -274,6 +275,7 @@
                     Laboratorio Nacional de Supercomputo del Sureste de Mexico
             </div>
         </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
