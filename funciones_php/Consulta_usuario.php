b4a9d92bc1818493a8e5a70a1d9a8c06619da6d0
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-16 13:36:48 -0600

    Cambio de estilos

--- funciones_php/Consulta_usuario.php
+++ funciones_php/Consulta_usuario.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <style> @import url('../css_estilos/propiedades_menu_inicial.css'); </style>
+        <style> @import url('../css_estilos/estilos.css'); </style>
     </head>
     <body>
     <?php
