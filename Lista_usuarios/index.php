98f0c196f5fc8f77961154afc0f27f927b845241
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-18 12:35:44 -0600

    Tabla Responsiva

--- Lista_usuarios/index.php
+++ Lista_usuarios/index.php
@@ -9,7 +9,7 @@
     </head>
     <body>
 
-        <?php
+          <?php
         include_once '../include/generar_tabla_usuarios.php';
         include_once '../funciones_php/Configuracion_sesion.php';
         $obj_gen_tab_usu = new Generar_Tabla_Usu;
@@ -17,12 +17,12 @@
 
         <div class="p-3 text-center text-white" id="banner">
             <div class="row">
-                    <div class="col d-flex justify-content-start">
-                    <img src="../LNS.png" height="64" width="170">
+                    <div class="col lns-logo d-flex justify-content-start">
+                    <img src="../LNS.png">
                 </div>
                
-                <div class="col d-flex justify-content-end">
-                    <img src="../buap-negativo.png" height="64" width="64">
+                <div class="col minerva d-flex justify-content-end">
+                    <img src="../buap-negativo.png">
                 </div>
             </div>
         </div>
@@ -56,54 +56,80 @@
                 <h1>Lista de usuarios</h1>
             </div>
 
-            <form action=""><div class="container text-center">
-                <div class="table-responsive-xxl">
-                    <table class="table table-light table-bordered table-hover">
-                        <thead>
-                            <tr>
-                            <th scope="col">Id Usuario</th>
-                            <th scope="col">Nombre de usuario</th>
-                            <th scope="col">Tipo de usuario</th>
-                            <th scope="col">Contraseña del usuario</th>
-                            <th></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php $obj_gen_tab_usu->generar_tabla_usuarios(); ?>
-                        </tbody>
-                    </table>
+            <form action="">
+                <div class="contenedorTabla">
+                    <div class="table-responsive-xxl">
+                        <table class="table table-light table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">Id Usuario</th>
+                                <th scope="col">Nombre de usuario</th>
+                                <th scope="col">Tipo de usuario</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $obj_gen_tab_usu->generar_tabla_usuariosT1(); ?>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="table-responsive-xxl">
+                        <table class="table table-light table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">Contraseña del usuario</th>
+                                <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $obj_gen_tab_usu->generar_tabla_usuariosT2(); ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div></form>
+            </form>
 
             <!-- FORMULARIO PARA CAMBIAR CONTRASENA DE USUARIO -->
             <form class="needs-validation" action="../funciones_php/Actualizacion_User.php" method="post" novalidate>
                 <div class="modal fade" id="actualizar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
                     <div class="modal-dialog modal-dialog-centered">
+
                         <div class="modal-content">
+
                             <div class="modal-header bg-info text-white">
                                 <h1 class="modal-title fs-5" id="exampleModalLabel">Escriba una nueva contraseña.</h1>
                             </div>
+
                             <div class="modal-body">
+
                                 <div class="container-fluid text-center" id="tabladatosusuarios"></div>
+
                                 <div data-mdb-input-init class="form-outline flex-fill mb-0">
                                     <input type="password" name="nueva_contrasenna" class="form-control" required/>
                                     <label class="form-label" for="nueva_contrasenna">Contraseña</label>
                                     <div class="valid-feedback">¡Se ve bien!</div>
                                     <div class="invalid-feedback">Por favor, Escriba una contraseña.</div>
                                 </div>
+
                                 <div data-mdb-input-init class="form-outline flex-fill mb-0">
                                     <input type="password" name="nueva_contrasenna" class="form-control" required/>
                                     <label class="form-label" for="nueva_contrasenna">Repita su contraseña</label>
                                     <div class="valid-feedback">¡Se ve bien!</div>
                                     <div class="invalid-feedback">Por favor, repita la contraseña</div>
                                 </div>
+
                             </div>
+
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
                                 <button class="btn btn-success" type="submit" data-bs-dismiss="modal">Aceptar</button>
                             </div>
+
                         </div>
+
                     </div>
+
                 </div>
             </form>
             <!--------------------------------------------------------------------------------------------------------->
@@ -135,6 +161,7 @@
                     Laboratorio Nacional de Supercomputo del Sureste de Mexico
             </div>
         </footer>
+        
     </body>
     <script src="../scripts_mantenimiento/scripts_consultas.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
