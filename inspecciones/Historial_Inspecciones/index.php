2b2bec54f058cbe26f2e07164a0accd5e9a9478d
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-18 13:23:19 -0600

    Estilos de resultados responsivos

--- inspecciones/Historial_Inspecciones/index.php
+++ inspecciones/Historial_Inspecciones/index.php
@@ -5,29 +5,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-        <style> @import url('../../css_estilos/menu_inicial.css'); </style>
-        <link rel="stylesheet" href="../../css_estilos/pantalla.css">
+        <style> @import url('estilos.css'); </style>
         <?php require '../../funciones_php/Configuracion_sesion.php'; ?>
     </head>
     <body>
         <div class="p-3 text-center text-white" id="banner">
             <div class="row">
-                <div class="col d-flex justify-content-start">
-                    <img src="../../LNS.png" height="64" width="170">
+                <div class="col lns-logo d-flex justify-content-start">
+                    <img src="../../LNS.png">
                 </div>
-               
-                <div class="col d-flex justify-content-end">
-                    <img src="../../buap-negativo.png" height="64" width="64">
+                
+                <div class="col minerva d-flex justify-content-end">
+                    <img src="../../buap-negativo.png">
                 </div>
             </div>
         </div>
-        <nav class="banner2 navbar-expand">
-            <div class="nav navbar-nav">
-                <a class="btn btn-lg" id="botones-lat" data-bs-toggle="modal" data-bs-target="#regreso" aria-current="step">
-                    Regresar 
-                </a>
-            </div>
-        </nav>
+        
         <div class="modal" id="regreso">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -61,53 +54,58 @@
                 </div>
             </div>
         </div>
-        <br>
+ 
+        <nav class="banner2 navbar-expand">
+            <div class="nav navbar-nav">
+                <a class="btn btn-lg" id="botones-lat" data-bs-toggle="modal" data-bs-target="#regreso" aria-current="step">
+                    Regresar 
+                </a>
+            </div>
+        </nav>
         <main class="content">
-        <?php
-        include_once '../../include/obtener_historial_registro.php';
-        $obj_obt_his_reg = new Obtener_Hist_Registro;
-        ?>
+
         <div class="txt-title">
             <h1>Historial de registros diarios</h1>
         </div>
 
-        <div class="container text-center">
-            <h5 class="text-center">Tabla de registros</h5>
-            <form action="../../include/inspeccion_registro.php" method="get">
-            <div
-                class="table-responsive"
-            >
-                <table
-                    class="table table-light table-hover"
+        <?php
+            include_once '../../include/obtener_historial_registro.php';
+            $obj_obt_his_reg = new Obtener_Hist_Registro;
+        ?>
+            <div class="container text-center">
+                <h5 class="text-center">Tabla de registros</h5>
+                <form action="../../include/inspeccion_registro.php" method="get">
+                <div
+                    class="table-responsive"
                 >
-                    <thead>
-                        <tr>
-                            <th scope="col">ID              </th>
-                            <th scope="col">Dia de semana   </th>
-                            <th scope="col">Fecha           </th>
-                            <th scope="col">Entrada o salida</th>
-                            <th scope="col">                </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $obj_obt_his_reg->obtener_historial_registro(); ?>
-                    </tbody>
-                </table>
+                    <table
+                        class="table table-light table-hover"
+                    >
+                        <thead>
+                            <tr>
+                                <th scope="col">ID              </th>
+                                <th scope="col">Dia de semana   </th>
+                                <th scope="col">Fecha           </th>
+                                <th scope="col">Entrada o salida</th>
+                                <th scope="col">                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $obj_obt_his_reg->obtener_historial_registro(); ?>
+                        </tbody>
+                    </table>
+                </div>
+                </form>
             </div>
-            </form>
-        </div>
         </main>
-    <footer class="bg-body-tertiary text-center">
-        <!-- Grid container -->
-         
-        <!-- Grid container -->
-      
-        <!-- Copyright -->
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-          Benemerita Universidad Autonoma de Puebla:
-          <a class="text-body">Laboratorio Nacional de Supercomputo del Sureste de Mexico</a>
+    
+
+    <footer class="text-center">
+        <div class="text-center p-3">
+                Benemerita Universidad Autonoma de Puebla:
+                Laboratorio Nacional de Supercomputo del Sureste de Mexico
+            
         </div>
-        <!-- Copyright -->
     </footer>
     <script src="../../scripts_mantenimiento/scripts_rutinarios.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
