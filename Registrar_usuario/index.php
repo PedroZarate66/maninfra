6c4df78a60435bdc857f45b4774a24837004a939
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-18 11:21:13 -0600

    Formulario responsivo

--- Registrar_usuario/index.php
+++ Registrar_usuario/index.php
@@ -8,28 +8,34 @@
         <link rel="stylesheet" href="../css_estilos/menu_inicial.css">
     </head>
     <body>
+
         <?php
         require '../funciones_php/Configuracion_sesion.php';
         ?>
+
+                <!--BANNER PRINCIPAL-->
         <div class="p-3 text-center text-white" id="banner">
             <div class="row">
-                <div class="col d-flex justify-content-start">
-                    <img src="../LNS.png" height="64" width="170">
+                <div class="col lns-logo d-flex justify-content-start">
+                    <img src="../LNS.png">
                 </div>
                 
-                <div class="col d-flex justify-content-end">
-                    <img src="../buap-negativo.png" height="64" width="64">
+                <div class="col minerva d-flex justify-content-end">
+                    <img src="../buap-negativo.png">
                 </div>
             </div>
             
         </div>
-        <nav class="banner2 navbar-expand">
-            <div class="nav navbar-nav">
+
+                <!--BANNER SECUNDARIO-->
+        <nav class="banner2">
+            <div class="nav">
                 <a class="btn btn-lg" id="botones-lat" data-bs-toggle="modal" data-bs-target="#regreso" aria-current="step"
                     >Regresar</a>
             </div>
         </nav>
         
+                <!--VENTANA EMERGENTE CUANDO SE QUIERE SALIR-->
         <div class="modal" id="regreso">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -46,102 +52,103 @@
                 </div>
             </div>
         </div>
-        <main class="content">   
+        
+
+            <!--INICIO DE CONTENIDO PRINCIPAL-->
+        <main class="content">
+
+                    <!--SUBTITULO-->
             <div class="txt-title">
                 <h1>Registrar usuario</h1>
             </div>
-            
-            <div class="container h-100">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-lg-12 col-xl-11">
-                        <div class="card text-black" style="border-radius: 25px;">
-                            <div class="card-body p-md-5">
-                                <div class="row justify-content-center">
-                                    <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                        <!-- <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registrar Usuario</p> -->
 
-                                        <form class="mx-1 mx-md-4 needs-validation" action="../funciones_php/Guardar_usuario.php" method="post" novalidate>
+                    <!--CONTENEDOR PRINCIPAL-->
+            <div class="container text-center">
 
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="text" name="nuevo_nombre" class="form-control" required/>
-                                                    <label class="form-label" for="nuevo_nombre">Nombre de usuario</label>
-                                                    <div class="valid-feedback">¡Se ve bien!</div>
-                                                    <div class="invalid-feedback">Por favor, Escriba un nombre de usuario.</div>
-                                                </div>
-                                            </div>
+            <?php
+                if (isset($_GET['mensaje']) && $_GET['mensaje'] == 'usuario_guardado') {
+                    echo 'Usuario creado con exito.';
+                }
+            ?>
 
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <p>Tipo de usuario</p>
-                                                    <input type="radio" class="btn-check" name="tipo_usuario" id="Administrador" value="Administrador" autocomplete="off" required>
-                                                    <label class="btn btn-outline-primary" for="Administrador">Administrador</label>
-    
-                                                    <input type="radio" class="btn-check" name="tipo_usuario" id="User_estandar" value="Normal" autocomplete="off" required>
-                                                    <label class="btn btn-outline-primary" for="User_estandar">Normal</label>
-                                                    
-                                                    <div class="valid-feedback">¡Se ve bien!</div>
-                                                    <div class="invalid-feedback">Por favor, seleccione tipo de usuario.</div>
-                                                </div>
-                                            </div>
+            </div>
 
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="password" name="nueva_contrasenna" class="form-control" required/>
-                                                    <label class="form-label" for="nueva_contrasenna">Contraseña</label>
-                                                    <div class="valid-feedback">¡Se ve bien!</div>
-                                                    <div class="invalid-feedback">Por favor, Escriba una contraseña.</div>
-                                                </div>
-                                            </div>
+            <div class="contenedorPrincipal">
+                <div class="contenedorFormulario">
 
-                                            <div class="d-flex flex-row align-items-center mb-4">
-                                                <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                                                <div data-mdb-input-init class="form-outline flex-fill mb-0">
-                                                    <input type="password" name="nueva_contrasenna" class="form-control" required/>
-                                                    <label class="form-label" for="nueva_contrasenna">Repita su contraseña</label>
-                                                    <div class="valid-feedback">¡Se ve bien!</div>
-                                                    <div class="invalid-feedback">Por favor, repita la contraseña</div>
-                                                </div>
-                                            </div>
+                                <!--FORMULARIO DE REGISTRO-->
+                    <form class="mx-1 mx-md-4 needs-validation" action="../include/registrar_usuario.php" method="post" novalidate>
 
-                                            <div class="form-check d-flex justify-content-center mb-5">
-                                                
-                                            </div>
-
-                                            <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg">Registrar</button>
-                                            </div>
-
-                                        </form>
-
-                                    </div>
-                                    <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center justify-item-center order-1 order-lg-2">
-                                        <img src="../favicon.ico" class="img-fluid" alt="Sample image">
-                                    </div>
+                        <div class="contentInput d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                                <div data-mdb-input-init class="input form-outline flex-fill mb-0">
+                                    <input type="text" name="nuevo_nombre" class="form-control" required/>
+                                    <label class="form-label" for="nuevo_nombre">Nombre de usuario</label>
+                                    <div class="valid-feedback">¡Se ve bien!</div>
+                                    <div class="invalid-feedback">Por favor, Escriba un nombre de usuario.</div>
                                 </div>
-                            </div>
                         </div>
-                    </div>
+
+                        <div class="contentInput d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                                <div data-mdb-input-init class="input form-outline flex-fill mb-0">
+                                    <p>Tipo de usuario</p>
+                                    <input type="radio" class="btn-check" name="tipo_usuario" id="Administrador" value="Administrador" autocomplete="off" required>
+                                    <label class="btn btn-outline-primary" for="Administrador">Administrador</label>
+        
+                                    <input type="radio" class="btn-check" name="tipo_usuario" id="User_estandar" value="Normal" autocomplete="off" required>
+                                    <label class="btn btn-outline-primary" for="User_estandar">Normal</label>
+                                                        
+                                    <div class="valid-feedback">¡Se ve bien!</div>
+                                    <div class="invalid-feedback">Por favor, seleccione tipo de usuario.</div>
+                                </div>
+                        </div>
+
+                        <div class="contentInput d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                                <div data-mdb-input-init class="input form-outline flex-fill mb-0">
+                                    <input type="password" name="nueva_contrasenna" class="form-control" required/>
+                                    <label class="form-label" for="nueva_contrasenna">Contraseña</label>
+                                    <div class="valid-feedback">¡Se ve bien!</div>
+                                    <div class="invalid-feedback">Por favor, Escriba una contraseña.</div>
+                                </div>
+                        </div>
+
+                        <div class="contentInput d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                                <div data-mdb-input-init class="input form-outline flex-fill mb-0">
+                                    <input type="password" name="nueva_contrasenna" class="form-control" required/>
+                                    <label class="form-label" for="nueva_contrasenna">Repita su contraseña</label>
+                                    <div class="valid-feedback">¡Se ve bien!</div>
+                                    <div class="invalid-feedback">Por favor, repita la contraseña</div>
+                                </div>
+                        </div>
+
+                        <div class="input d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                            <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg">Registrar</button>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="contenedorIcono">
+                    <img src="../favicon.ico" class="img-fluid" alt="Sample image">
                 </div>
             </div>
         </main>
-        <footer class="bg-body-tertiary text-center">
-            <!-- Grid container -->
-        
-            <!-- Grid container -->
 
-            <!-- Copyright -->
-            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-                Benemerita Universidad Autonoma de Puebla:
-                <a class="text-body">Laboratorio Nacional de Supercomputo del Sureste de Mexico</a>
+        <footer class="text-center">
+            <div class="text-center p-3">
+                    Benemerita Universidad Autonoma de Puebla:
+                    Laboratorio Nacional de Supercomputo del Sureste de Mexico
+                
             </div>
-        <!-- Copyright -->
         </footer>
+
     </body>
+
     <script src="../scripts_mantenimiento/scripts_consultas.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
