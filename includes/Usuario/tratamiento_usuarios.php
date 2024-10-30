c4c3a36401ae4a3cf09b8d3643e80fd1da1bf2b9
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-30 10:24:54 -0600

    cambios de ruta

--- includes/Usuario/tratamiento_usuarios.php
+++ includes/Usuario/tratamiento_usuarios.php
@@ -1,16 +1,16 @@
 <?php
-include_once 'tratamiento_constrasenna.php';
-
+include_once 'tratamiento_contrasenna.php';
+ 
 class Tratamiento_Usuarios{
     public function TratamientoUsuarios()
         {
             $obj_trat_cont = new Tratamiento_Contrasenna;
-
             $entradainfra = array();
             $entradainfra[0] = $_POST["nuevo_nombre"];
             $entradainfra[1] = $_POST["tipo_usuario"];
             $entradainfra[2] = $obj_trat_cont->TratamientoContrasenna();
             $consultasql = "INSERT INTO `mantinfrausuarios` (`nombreUsuario`, `tipoUsuario`, `contrasennaUsuario`) VALUES ('$entradainfra[0]', '$entradainfra[1]', '$entradainfra[2]');";
+            
             return $consultasql;
         }
     }
