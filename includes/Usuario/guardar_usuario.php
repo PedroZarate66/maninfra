1d3fa6fb06abe2645c2f71665141020d042a289d
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-30 10:24:21 -0600

    Cambios de rutas

--- includes/Usuario/guardar_usuario.php
+++ includes/Usuario/guardar_usuario.php
@@ -1,16 +1,20 @@
 <?php
-include_once '../../include/conexion.php';
+include_once '../conexion.php';
 include_once 'tratamiento_usuarios.php';
 
 class Guardar_Usu{
     public function guardar_usuario()
     {
-        $conexion = new Crear_Conexion;
+        $conexion     = new Crear_Conexion;
+        
         $obj_trat_usu = new Tratamiento_Usuarios;
-
+        
+        
         $conexion->crear_conection();
 
         $sql = $obj_trat_usu->TratamientoUsuarios();
+        
+
         if ($conexion->conexionBD->query($sql) === TRUE){
             echo "registro guardado<br>";
         }else {
