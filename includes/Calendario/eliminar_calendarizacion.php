9db1066afa73fa1a95656e9fa3ba3e562091b524
PedroZarate66 <38222769+PedroZarate66@users.noreply.github.com>
2024-10-30 12:16:16 -0600

    Correccion de redireccionamiento

--- includes/Calendario/eliminar_calendarizacion.php
+++ includes/Calendario/eliminar_calendarizacion.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <style> @import url('../../css_estilos/menu_inicial.css'); </style>
+        <style> @import url('estilos.css'); </style>
     </head>
     <body>
     <?php
